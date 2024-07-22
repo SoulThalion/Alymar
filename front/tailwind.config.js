2faa40d924648ac54ad88b089b0eb2e8c19b9c48
SoulThalion <adrielguijarro@gmail.com>
2024-07-22 15:33:33 +0100

    Vite limpio y tailwind instalado

---
+++ front/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+      '8': '8px',
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
